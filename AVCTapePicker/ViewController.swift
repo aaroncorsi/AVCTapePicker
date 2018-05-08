c608e9f977d6a89c039f1aa87419661193e6fda6
Aaron Corsi <me@aaroncorsi.com>
2018-05-07 21:05:40 -0400

    Initial Commit

---
+++ AVCTapePicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AVCTapePicker
+//
+//  Created by Aaron Corsi on 5/7/18.
+//  Copyright © 2018 Aaron Corsi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
